25baca06eab7fa4e4d5400e4fbb5b71f0ae2ac80
rchiky <raja.chiky@isep.fr>
2015-09-10 14:14:21 +0200

    ajout

---
+++ src/TestWordSpout.java
@@ -0,0 +1,69 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import backtype.storm.topology.OutputFieldsDeclarer;
+
+import java.util.Map;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import backtype.storm.utils.Utils;
+
+import java.util.Random;
+
+
+
+public class TestWordSpout extends BaseRichSpout {
+ 
+    SpoutOutputCollector _collector;
+
+             
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+        _collector = collector;
+    }
+    
+    public void close() {
+        
+    }
+        
+    public void nextTuple() {
+        Utils.sleep(100);
+        final String[] words = new String[] {"nathan", "mike", "jackson", "golda", "bertels"};
+        final Random rand = new Random();
+        final String word = words[rand.nextInt(words.length)];
+        _collector.emit(new Values(word));
+    }
+    
+    public void ack(Object msgId) {
+
+    }
+
+    public void fail(Object msgId) {
+        
+    }
+    
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word"));
+    }
+
+   
+}
